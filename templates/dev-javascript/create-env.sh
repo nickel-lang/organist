0c3e80e805987f8e2dd34a9b79b94909c34d3dae
Daniel Baker <daniel.baker@tweag.io>
2023-02-01 15:54:24 +0100

    modify create env so it doesn't overwrite current directory. also copy itself to the other directories.

---
+++ templates/dev-javascript/create-env.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+for devenv in $(awk '/= BashShell/ { print $1 }' builders.ncl)
+do
+  ENV_NAME="dev-$(echo $devenv | tr [A-Z] [a-z] | sed 's/shell$//')"
+  DEST="../$ENV_NAME"
+  if ! [[ ./ -ef "$DEST" ]];
+  then
+    echo $ENV_NAME
+
+    rm -fr "$DEST"
+    mkdir -p "$DEST"
+    cp builders.ncl contracts.ncl flake.*  naked-stdenv.ncl naked-stdenv.sh create-env.sh "$DEST"
+
+    cat <<EOF > "${DEST}/dev-shell.ncl"
+let builders = import "builders.ncl" in
+
+{
+  output = {
+    name = "nickel-shell",
+  }
+} & builders.$devenv
+EOF
+  fi
+done
