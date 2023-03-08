29574528b0de5c9f270a8d6dbae2b4380899dbec
Daniel Baker <daniel.baker@tweag.io>
2023-03-08 16:34:05 +0100

    initial commit of working haskell.

---
+++ templates/devshells/haskell/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A basic flake with a shell";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nickel-nix.url = "github:nickel-lang/nickel-nix";
+
+  nixConfig = {
+    extra-substituters = ["https://tweag-nickel.cachix.org"];
+    extra-trusted-public-keys = ["tweag-nickel.cachix.org-1:GIthuiK4LRgnW64ALYEoioVUQBWs0jexyoYVeLDBwRA="];
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nickel-nix,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      importNcl = nickel-nix.lib.${system}.importNcl;
+      nakedStdenv = importNcl ./nixel "naked-stdenv.ncl" inputs;
+      nickelDerivation =
+        importNcl ./. "dev-shell.ncl"
+        (
+          inputs
+          // {
+            nixel-internals.packages.${system} = {naked_std_env = nakedStdenv;};
+          }
+        );
+    in rec {
+      packages = {
+        default = nakedStdenv;
+      };
+      devShells = {
+        default = nickelDerivation;
+      };
+    });
+}
