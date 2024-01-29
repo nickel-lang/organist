7e6c7f8a1284e00da55d2d27bdc6890e48b9af1e
Théophane Hufschmitt <7226587+thufschmitt@users.noreply.github.com>
2024-01-29 18:29:38 +0100

    Fix a typo in the error message for a missing input

--- lib/lib.nix
+++ lib/lib.nix
@@ -106,7 +106,7 @@
               ([value.input "packages" system] ++ attr_path)
               ([value.input "legacyPackages" system] ++ attr_path)
             ];
-            notFound = throw "Missing input \"${value.input}.${lib.strings.concatStringsSep "." attr_path}\"";
+            notFound = throw "Missing input \"${value.input}#${lib.strings.concatStringsSep "." attr_path}\"";
             chosenAttrPath =
               lib.findFirst
               (path: lib.hasAttrByPath path flakeInputs)
