dd1ceb25ac352ee4ce24b5d08fbbfb9dbe7f924d
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2024-05-24 08:33:31 +0200

    Regenerate the files transparently when entering the dev shell
    Removes the need from running `regenerate-files` manually

--- run-test.sh
+++ run-test.sh
@@ -120,7 +120,7 @@ test_example () (
   cp -r "$examplePath" ./example
   pushd ./example
   prepare_shell
-  nix run .\#regenerate-files --print-build-logs
+  # nix run .\#regenerate-files --print-build-logs
   nix develop --print-build-logs --command bash test.sh
   popd
   popd
