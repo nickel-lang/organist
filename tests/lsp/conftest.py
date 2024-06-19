594cc2a6cba44f4b6b0358f6444829fef2e135c0
Théophane Hufschmitt <theophane.hufschmitt@tweag.io>
2024-06-19 11:35:11 +0200

    Add tests for the lsp integration
    Ensure that autocompletion works fine for simple things

---
+++ tests/lsp/conftest.py
@@ -0,0 +1,21 @@
+import testlib
+import asyncio
+import pytest
+import pytest_asyncio
+from lsprotocol import types as lsp
+
+@pytest_asyncio.fixture
+async def client():
+    # Setup
+    client = testlib.LanguageClient("organist-test-suite", "v1")
+    await client.start_io("nls")
+    response = await client.initialize_async(
+            lsp.InitializeParams(
+                capabilities=lsp.ClientCapabilities(),
+                root_uri="."
+            )
+        )
+    assert response is not None
+    client.initialized(lsp.InitializedParams())
+    return client
+
